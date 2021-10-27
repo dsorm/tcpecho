0772bbf516f59441d30f17bcada4423ca1e456d4
David Sorm <david.sorm@gmail.com>
2021-10-27 23:52:52 +0200

    tests and benchmarks

---
+++ common/server_test.go
@@ -0,0 +1,53 @@
+package common
+
+import (
+	"bytes"
+	"testing"
+	"time"
+)
+
+const ServerAddress = ":7000"
+
+func TestMain(m *testing.M) {
+	ServerStart(ServerAddress)
+	m.Run()
+}
+
+func TestServerStart(t *testing.T) {
+	expected := []byte("Test!")
+	got := SendEcho(expected, ServerAddress, 2*time.Second)
+
+	// if not equal
+	if bytes.Compare(expected, got) != 0 {
+		t.Fatalf("Error while testing server: got %v, expected %v", string(got), string(expected))
+	}
+}
+
+func Benchmark_1x(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		SendEcho([]byte("Hello world!"), ServerAddress, time.Second)
+	}
+}
+
+func Benchmark_64x(b *testing.B) {
+	running := true
+	start := make(chan bool, 1)
+	addOne := make(chan bool, 16)
+	var i int
+
+	for g := 0; g < 64; g++ {
+		go func() {
+			<-start
+			for running {
+				SendEcho([]byte("Hello world!"), ServerAddress, time.Second)
+				addOne <- true
+			}
+		}()
+	}
+
+	start <- true
+	for i = 0; i < b.N; i++ {
+		<-addOne
+	}
+	running = false
+}
